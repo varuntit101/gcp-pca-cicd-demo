222c173e9a532686b5f165f9dd30c31b2bcf88f9
varun agarwal <varun@varuns-MacBook-Pro.local>
2025-07-27 19:02:02 +0530

    Update homepage text

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "🚀 CI/CD deployed Flask app!"
+    return "🚀 CI/CD deployed Flask app! updated"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
